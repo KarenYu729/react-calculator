cc353c995715e8f1033f41800ea688cc88f71d4f
Jiahui Yu <97644710+KarenYu729@users.noreply.github.com>
2023-07-14 20:00:28 -0500

    Add files via upload

---
+++ src/DigitButton.js
@@ -0,0 +1,11 @@
+import { ACTIONS } from "./App"
+// pass dispatch as a param -> call reducer from App.js
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
